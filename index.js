9b424a62ea57766c7a2ab578b8c1f622510e2a6d
EPRedstoneM <16shammer@jess.sch.ae>
2021-01-02 12:34:17 +0000

    Create the bot

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Bot online')
+})
+
+client.on('message', message => {
+  if (message.content.includes("r!rng")) {
+    const args = message.content.replace('r!rng', '')
+    if (!args) {
+      message.channel.send('What do you want as the highest possible number?')
+      return
+    }
+    if (isNaN(args)) {
+      message.channel.send('Please provide a valid number.')
+      return
+    }
+    
+    const answ = args - 1
+    
+    message.channel.send(`${1 + Math.floor(Math.random() * answ)}`)
+  }
+})
+
+client.login(process.env.TOKEN)
